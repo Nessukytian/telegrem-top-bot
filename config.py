88cbcf229fd6f8f8d86891c9293995f5587fac31
Nessukytian <58769367+Nessukytian@users.noreply.github.com>
2025-08-07 15:52:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # читает .env из корня
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+API_ID    = int(os.getenv("API_ID"))
+API_HASH  = os.getenv("API_HASH")
+OWNER_ID  = int(os.getenv("OWNER_ID"))
