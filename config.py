7a109deb0efc47643cf762c61bee7431d72b4147
Nessukytian <58769367+Nessukytian@users.noreply.github.com>
2025-08-07 18:34:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-OWNER_ID = int(os.getenv("OWNER_ID"))
+BOT_TOKEN      = os.getenv("BOT_TOKEN")
+API_ID         = int(os.getenv("API_ID"))
+API_HASH       = os.getenv("API_HASH")
+OWNER_ID       = int(os.getenv("OWNER_ID"))
+STRING_SESSION = os.getenv("STRING_SESSION")
